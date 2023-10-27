4e86acb224d514203f9c0c03ddc85cfce520ad75
cshea15 <chashea@microsoft.com>
2023-10-27 15:54:41 -0400

    update changes to file

--- main.tf
+++ main.tf
@@ -1,1 +1,43 @@
 # TODO: insert resources here.
+// Create Azure Firewall Policy
+
+resource "azurerm_firewall_policy" "firewall_policy" {
+  name                = var.fw_policy_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  sku                 = var.sku
+  dns {
+    proxy_enabled = var.proxy_enabled
+    servers       = var.dns_servers
+  }
+  threat_intelligence_mode = var.threat_intel_mode
+  threat_intelligence_allowlist {
+    fqdns        = var.threat_intel_fqdn_allowlist
+    ip_addresses = var.threat_intel_ip_allowlist
+  }
+  tags = var.tags
+}
+
+
+
+
+
+resource "azurerm_management_lock" "this" {
+  count      = var.lock.kind != "None" ? 1 : 0
+  name       = coalesce(var.lock.name, "lock-${var.fw_policy_name}")
+  scope      = azurerm_virtual_network.vnet.id
+  lock_level = var.lock.kind
+}
+
+# Assigning Roles to the Virtual Network based on the provided configurations.
+resource "azurerm_role_assignment" "this" {
+  for_each                               = var.role_assignments
+  scope                                  = azurerm_virtual_network.vnet.id
+  role_definition_id                     = strcontains(lower(each.value.role_definition_id_or_name), lower(local.role_definition_resource_substring)) ? each.value.role_definition_id_or_name : null
+  role_definition_name                   = strcontains(lower(each.value.role_definition_id_or_name), lower(local.role_definition_resource_substring)) ? null : each.value.role_definition_id_or_name
+  principal_id                           = each.value.principal_id
+  condition                              = each.value.condition
+  condition_version                      = each.value.condition_version
+  skip_service_principal_aad_check       = each.value.skip_service_principal_aad_check
+  delegated_managed_identity_resource_id = each.value.delegated_managed_identity_resource_id
+}
--- variables.tf
+++ variables.tf
@@ -13,3 +13,121 @@ variable "resource_group_name" {
   type        = string
   description = "The resource group where the resources will be deployed."
 }
+
+variable "location" {
+  type        = string
+  description = "The Azure region where the resources will be deployed."
+}
+
+variable "fw_policy_name" {
+  type        = string
+  description = "The name of the Azure Firewall Policy."
+}
+
+variable "sku" {
+  type        = string
+  description = "The Azure Firewall Policy SKU."
+  validation {
+    condition     = contains(["Basic", "Standard", "Premium"], var.sku)
+    error_message = "The SKU must be one of the following: Basic, Standard, Premium"
+  }
+}
+
+variable "proxy_enabled" {
+  type        = bool
+  description = "This variable controls whether or not telemetry is enabled for the module."
+}
+
+variable "dns_servers" {
+  type        = list(string)
+  description = <<DESCRIPTION
+  The list of DNS servers to use for the Azure Firewall Policy.
+  This will not be needed if the DNS proxy enabled is set to False
+  DESCRIPTION
+}
+
+variable "threat_intel_mode" {
+  type        = string
+  description = "The threat intelligence mode for the Azure Firewall Policy."
+
+  validation {
+    condition     = contains(["Alert", "Deny", "Off"], var.threat_intel_mode)
+    error_message = "The threat intelligence mode must be one of the following: Alert, Deny, Off"
+  }
+}
+
+variable "threat_intel_fqdn_allowlist" {
+  type        = list(string)
+  description = "The list of FQDNs to allow for the Azure Firewall Policy."
+}
+
+variable "threat_intel_ip_allowlist" {
+  type        = list(string)
+  description = "describe your variable"
+}
+
+
+
+variable "diagnostic_settings" {
+  type = map(object({
+    name                                     = optional(string, null)
+    log_categories_and_groups                = optional(set(string), ["allLogs"])
+    metric_categories                        = optional(set(string), ["AllMetrics"])
+    log_analytics_destination_type           = optional(string, "Dedicated")
+    workspace_resource_id                    = optional(string, null)
+    storage_account_resource_id              = optional(string, null)
+    event_hub_authorization_rule_resource_id = optional(string, null)
+    event_hub_name                           = optional(string, null)
+    marketplace_partner_resource_id          = optional(string, null)
+  }))
+  default  = {}
+  nullable = false
+
+  validation {
+    condition     = alltrue([for _, v in var.diagnostic_settings : contains(["Dedicated", "AzureDiagnostics"], v.log_analytics_destination_type)])
+    error_message = "Log analytics destination type must be one of: 'Dedicated', 'AzureDiagnostics'."
+  }
+}
+
+
+variable "role_assignments" {
+  type = map(object({
+    role_definition_id_or_name             = string
+    principal_id                           = string
+    description                            = optional(string, null)
+    skip_service_principal_aad_check       = optional(bool, true)
+    condition                              = optional(string, null)
+    condition_version                      = optional(string, "2.0")
+    delegated_managed_identity_resource_id = optional(string)
+  }))
+  default = {}
+}
+
+variable "lock" {
+  type = object({
+    name = optional(string, null)
+    kind = optional(string, "None")
+
+
+  })
+  description = "The lock level to apply to the Virtual Network. Default is `None`. Possible values are `None`, `CanNotDelete`, and `ReadOnly`."
+  default     = {}
+  nullable    = false
+  validation {
+    condition     = contains(["CanNotDelete", "ReadOnly", "None"], var.lock.kind)
+    error_message = "The lock level must be one of: 'None', 'CanNotDelete', or 'ReadOnly'."
+  }
+}
+
+
+# Example resource implementation
+
+variable "tags" {
+  type = map(any)
+  default = {
+
+  }
+  description = <<DESCRIPTION
+The tags to associate with your network and subnets.
+DESCRIPTION
+}
