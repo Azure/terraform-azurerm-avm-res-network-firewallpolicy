3897f311ceb5fa9b52bfc8fbee35467e11d6e8c5
cshea15 <chashea@microsoft.com>
2024-05-29 14:10:11 -0400

    fixing local

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
-  enable_telemetry                   = true
+  enable_telemetry                   = var.enable_telemetry
   role_definition_resource_substring = "providers/Microsoft.Authorization/roleDefinitions"
 }
